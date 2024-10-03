cd25bd98ad93d4b426877da26c5c808d125aa0fe
Adam <bgisgone@gmail.com>
2024-10-04 00:28:52 +0800

    新增DALL·E API

---
+++ Chatbot/botImgTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  botImgTableViewCell.swift
+//  Chatbot
+//
+//  Created by Adam Chen on 2024/10/3.
+//
+
+import UIKit
+
+class botImgTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
